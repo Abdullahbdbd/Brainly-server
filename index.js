ae574c7473ae12c2dfcdc05d6815c126e13b139a
Abdullahbdbd <abdullahmohammad262000@gmail.com>
2023-06-09 23:16:10 +0600

    Delete API

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -10,7 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nmjlal4.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -56,6 +56,13 @@ async function run() {
        const result = await bookedCollection.insertOne(item)
        res.send(result);
     });
+
+    app.delete('/booked/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await bookedCollection.deleteOne(query);
+        res.send(result);
+    })
     
 
 
