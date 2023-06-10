a64c0fcf0116af94b26cf7bfdbec016eb6f07998
Abdullahbdbd <abdullahmohammad262000@gmail.com>
2023-06-10 22:08:40 +0600

    Admin Done

--- index.js
+++ index.js
@@ -12,6 +12,24 @@ app.use(cors());
 app.use(express.json());
 
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nmjlal4.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -41,9 +59,20 @@ async function run() {
     })
 
 
+    const verifyAdmin = async (req, res, next) => {
+        const email = req.decoded.email;
+        const query = { email: email }
+        const user = await usersCollection.findOne(query);
+        if (user?.role !== 'admin') {
+            return res.status(403).send({ error: true, message: 'Forbidden message' });
+        }
+        next();
+    }
+
+
     // Users Collection API
 
-    app.get('/users', async (req, res) => {
+    app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
         const result = await usersCollection.find().toArray();
         res.send(result);
     })
@@ -60,6 +89,20 @@ async function run() {
         console.log(result)
         res.send(result)
       })
+
+
+      app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+        const email = req.params.email;
+
+        if (req.decoded.email !== email) {
+            res.send({ admin: false })
+        }
+
+        const query = { email: email }
+        const user = await usersCollection.findOne(query);
+        const result = { admin: user?.role === 'admin' }
+        res.send(result);
+    })
 
       app.patch('/users/admin/:id', async (req, res) => {
         const id = req.params.id;
@@ -97,11 +140,17 @@ async function run() {
 
     // Booked collection API
 
-    app.get('/booked', async(req, res)=>{
+    app.get('/booked', verifyJWT, async(req, res)=>{
         const email = req.query.email;
         if(!email){
             res.send([]);
         }
+
+        const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ error: true, message: 'Forbidden access' });
+            }
+
         const query = {email: email};
         const result = await bookedCollection.find(query).toArray();
         res.send(result)
